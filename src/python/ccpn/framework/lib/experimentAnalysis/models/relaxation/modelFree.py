c7c26b1ad838725e5619fe0d3e063dc223819d02
Luca Mureddu <luca.mureddu@me.com>
2023-11-07 09:54:24 +0000

    Starting dir refactoring

---
+++ src/python/ccpn/framework/lib/experimentAnalysis/models/relaxation/modelFree.py
@@ -0,0 +1,30 @@
+"""
+This module defines base classes for Series Analysis
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2023"
+__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
+__reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
+                 "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
+#=========================================================================================
+# Last code modification
+#=========================================================================================
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2023-11-07 09:54:24 +0000 (Tue, November 07, 2023) $"
+__version__ = "$Revision: 3.2.0 $"
+#=========================================================================================
+# Created
+#=========================================================================================
+__author__ = "$Author: Luca Mureddu $"
+__date__ = "$Date: 2022-02-02 14:08:56 +0000 (Wed, February 02, 2022) $"
+#=========================================================================================
+# Start of code
+#=========================================================================================
+#================
+
+# todo
