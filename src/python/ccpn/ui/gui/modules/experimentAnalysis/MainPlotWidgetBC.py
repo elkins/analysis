18bae2279d39474f94a4d23ef4b171177381104c
Luca Mureddu <luca.mureddu@me.com>
2023-11-08 11:20:00 +0000

    Experiment Analysis. Bug fix on the exporter.

--- src/python/ccpn/ui/gui/modules/experimentAnalysis/MainPlotWidgetBC.py
+++ src/python/ccpn/ui/gui/modules/experimentAnalysis/MainPlotWidgetBC.py
@@ -11,8 +11,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2023-11-08 11:39:35 +0000 (Wed, November 08, 2023) $"
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2023-11-08 11:20:00 +0000 (Wed, November 08, 2023) $"
 __version__ = "$Revision: 3.2.0 $"
 #=========================================================================================
 # Created
