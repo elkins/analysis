77b1895242765541ede2ec946ad50cefbcc0651e
Luca Mureddu <luca.mureddu@me.com>
2024-09-27 10:12:57 +0100

    minor change on plotting Rex from Etas

--- src/python/ccpn/macros/relaxation/Calculate_RelaxationExchange_via_ETAs.py
+++ src/python/ccpn/macros/relaxation/Calculate_RelaxationExchange_via_ETAs.py
@@ -22,8 +22,9 @@ Evaluation of two simplified 15N-NMR methods for determining µs–ms dynamics o
 # Licence, Reference and Credits
 #=========================================================================================
 __copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2024"
-__credits__ = ("Ed Brooksbank, Joanna Fox, Morgan Hayward, Victoria A Higman, Luca Mureddu",
-               "Eliza Płoskoń, Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
+__credits__ = ("Ed Brooksbank, Morgan Hayward, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Daniel Thompson",
+               "Gary S Thompson & Geerten W Vuister")
 __licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
@@ -31,9 +32,9 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 #=========================================================================================
 # Last code modification
 #=========================================================================================
-__modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2024-04-04 15:19:21 +0100 (Thu, April 04, 2024) $"
-__version__ = "$Revision: 3.2.5 $"
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2024-09-27 10:12:57 +0100 (Fri, September 27, 2024) $"
+__version__ = "$Revision: 3.2.9.alpha $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -288,7 +289,7 @@ def _ploteExchangeRates(pdf):
     macrosLib._setXTicks(ax2, labelMajorSize, labelMinorSize)
     ax2.legend(loc='best', prop={'size': 4})
     ax2.spines[['right', 'top']].set_visible(False)
-    _setMargins(ax2, rexFromR2ETAxy)
+    _setMargins(ax2, rexFromR2R1)
 
     ## plot Secondary structure
     if macrosLib._isSequenceValid(sequence, ss_sequence):
