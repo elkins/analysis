3605305661e178d9cea8819d1fbe4c566303deb3
Ed Brooksbank <ejb66@le.ac.uk>
2024-12-20 10:52:31 +0000

    minor logic

--- src/python/ccpn/ui/gui/modules/PeakTable.py
+++ src/python/ccpn/ui/gui/modules/PeakTable.py
@@ -16,7 +16,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2024-12-09 17:18:44 +0000 (Mon, December 09, 2024) $"
+__dateModified__ = "$dateModified: 2024-12-20 10:52:31 +0000 (Fri, December 20, 2024) $"
 __version__ = "$Revision: 3.2.11 $"
 #=========================================================================================
 # Created
@@ -154,10 +154,9 @@ class PeakTableModule(CcpnTableModule):
     def _closeModule(self):
         """CCPN-INTERNAL: used to close the module
         """
-        if self.activePulldownClass:
-            if self._settings:
-                self._settings._cleanupWidget()
-                self._settings = None
+        if self.activePulldownClass and self._settings:
+            self._settings._cleanupWidget()
+            self._settings = None
         super()._closeModule()
 
     def _getLastSeenWidgetsState(self):
