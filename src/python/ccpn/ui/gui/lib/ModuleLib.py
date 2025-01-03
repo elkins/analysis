956825f4821c30d5473a4dd7942d1e88f8043560
Ed Brooksbank <ejb66@le.ac.uk>
2025-01-03 18:15:19 +0000

    removed old close-all-children

--- src/python/ccpn/ui/gui/lib/ModuleLib.py
+++ src/python/ccpn/ui/gui/lib/ModuleLib.py
@@ -4,7 +4,7 @@ Module Documentation here
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2024"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2025"
 __credits__ = ("Ed Brooksbank, Morgan Hayward, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Daniel Thompson",
                "Gary S Thompson & Geerten W Vuister")
@@ -16,7 +16,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2024-12-20 11:37:54 +0000 (Fri, December 20, 2024) $"
+__dateModified__ = "$dateModified: 2025-01-03 18:15:19 +0000 (Fri, January 03, 2025) $"
 __version__ = "$Revision: 3.2.11 $"
 #=========================================================================================
 # Created
@@ -27,16 +27,15 @@ __date__ = "$Date: 2024-05-16 11:17:23 +0100 (Thu, May 16, 2024) $"
 # Start of code
 #=========================================================================================
 
-from PyQt5 import QtWidgets, QtCore, sip
+from PyQt5 import QtWidgets
 from ccpn.framework.Application import getApplication
-from ccpn.ui.gui.widgets.Base import Base
 from ccpn.ui.gui.widgets.SpeechBalloon import SpeechBalloon
 from ccpn.ui.gui.guiSettings import consoleStyle
 from ccpn.util.Logging import getLogger
 
 
 BLOCKINGDIALOGS = (QtWidgets.QDialog, QtWidgets.QMenu, SpeechBalloon)
-_DEBUG = True
+_DEBUG = False
 _RAISEERROR = False
 
 
@@ -65,33 +64,3 @@ def getBlockingDialogs(msg: str = None) -> bool:
             getLogger().debug(f'{consoleStyle.fg.yellow}==> {msg} {state}:{blocked}'
                               f'{consoleStyle.reset}')
         return blocked
-
-
-def _closeAllChildren(widget: QtWidgets.QWidget, *, depth: int = 4):
-    """
-    Recursively close all child widgets of a given widget.
-
-    This function closes all child widgets of the specified widget up to a certain depth.
-    It can optionally ignore closing the widget itself.
-
-    :param widget: The parent widget whose children will be closed.
-    :type widget: QtWidgets.QWidget
-    :param depth: The depth level for indentation in debug messages, defaults to 4.
-    :type depth: int, optional
-    """
-    _msg = f'{consoleStyle.fg.lightgrey}{" " * depth}=> {widget}'
-    for child in widget.findChildren(QtWidgets.QWidget, options=QtCore.Qt.FindDirectChildrenOnly):
-        _closeAllChildren(child, depth=depth + 4)
-        try:
-            if issubclass(child.__class__, Base):
-                child._close(depth=depth + 4)
-                _msg += f'{consoleStyle.fg.darkgreen} closed'
-        except AttributeError:
-            _msg += f'{consoleStyle.fg.red} error'
-            if _RAISEERROR:
-                raise
-        finally:
-            if not sip.isdeleted(child):
-                child.setParent(None)
-                child.deleteLater()
-    if _DEBUG: print(_msg, f'{consoleStyle.reset}')
