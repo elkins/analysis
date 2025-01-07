d94318266c103123aaa43c3cfb60db1ad26b53d6
Ed Brooksbank <ejb66@le.ac.uk>
2025-01-07 16:33:18 +0000

    cleaned up some _closeModule

--- src/python/ccpn/ui/gui/modules/PluginModule.py
+++ src/python/ccpn/ui/gui/modules/PluginModule.py
@@ -4,7 +4,7 @@
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2024"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2025"
 __credits__ = ("Ed Brooksbank, Morgan Hayward, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Daniel Thompson",
                "Gary S Thompson & Geerten W Vuister")
@@ -16,7 +16,7 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2024-11-26 10:38:14 +0000 (Tue, November 26, 2024) $"
+__dateModified__ = "$dateModified: 2025-01-07 16:33:19 +0000 (Tue, January 07, 2025) $"
 __version__ = "$Revision: 3.2.11 $"
 #=========================================================================================
 # Created
@@ -95,6 +95,7 @@ class PluginModule(CcpnModule):
 
     def _closeModule(self):
         self.closePlugin()
+        super()._closeModule()
 
     def issueMessage(self, message):
         raise NotImplemented('Messages are not implemented yet.')
