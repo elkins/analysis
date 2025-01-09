1c5c1c71cbf22baa91b65e32da0eaac983ce52d5
Ed Brooksbank <ejb66@le.ac.uk>
2025-01-09 15:51:15 +0000

    removed _table - deprecated module?

--- src/python/ccpn/ui/gui/widgets/RowExpander.py
+++ src/python/ccpn/ui/gui/widgets/RowExpander.py
@@ -4,19 +4,20 @@ Module Documentation here
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2021"
-__credits__ = ("Ed Brooksbank, Joanna Fox, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
-               "Timothy J Ragan, Brian O Smith, Gary S Thompson & Geerten W Vuister")
-__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2025"
+__credits__ = ("Ed Brooksbank, Morgan Hayward, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
+               "Timothy J Ragan, Brian O Smith, Daniel Thompson",
+               "Gary S Thompson & Geerten W Vuister")
+__licence__ = ("CCPN licence. See https://ccpn.ac.uk/software/licensing/")
 __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
                  "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
-                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+                 "J.Biomol.Nmr (2016), 66, 111-124, https://doi.org/10.1007/s10858-016-0060-y")
 #=========================================================================================
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2021-05-06 14:04:51 +0100 (Thu, May 06, 2021) $"
-__version__ = "$Revision: 3.0.4 $"
+__dateModified__ = "$dateModified: 2025-01-09 15:51:16 +0000 (Thu, January 09, 2025) $"
+__version__ = "$Revision: 3.2.11 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -47,19 +48,20 @@ class RowExpander(QtWidgets.QWidget):
         """
         super().__init__(parent)
 
-        self._table = parent
         self._tableItem = tableItem
         self._expanded = expanded
         self._activeRow = None
-
-        # create the pixmaps for the icon
+        # create the pix-maps for the icon
         self._openPixmap = Icon(self.OPENICON).pixmap(self.PIXMAPSIZE, self.PIXMAPSIZE)
         self._closedPixmap = Icon(self.CLOSEDICON).pixmap(self.PIXMAPSIZE, self.PIXMAPSIZE)
-
         self._setWidgets()
 
+    @property
+    def _parent(self):
+        return self.parent()
+
     def _setWidgets(self):
-        """Setup the widgets
+        """Set up the widgets
         """
         # create a new button from an active label - doesn't have a button-click visual effect
         button = self._button = ActiveLabel(self)
@@ -79,10 +81,10 @@ class RowExpander(QtWidgets.QWidget):
         if row is None:
             return
 
-        span = self._table.rowSpan(row, self._tableItem.column())
+        span = self._parent.rowSpan(row, self._tableItem.column())
         if span > 1:
             # a group of rows - set the state of the first row
-            if self._table.isRowHidden(row + 1):
+            if self._parent.isRowHidden(row + 1):
                 hidden = False
                 self._button.setPixmap(self._openPixmap)
             else:
@@ -91,14 +93,14 @@ class RowExpander(QtWidgets.QWidget):
 
             # change the state of the remaining rows
             for rr in range(row + 1, row + span):
-                self._table.setRowHidden(rr, hidden)
+                self._parent.setRowHidden(rr, hidden)
 
     def updateCellWidget(self, row, visible, setPixMapState=None):
         """Update the expanded/collapsed state of the button
 
         :param row: First row of the group of spanned cells this cell is associated with
         :param visible: True/False - cell visibility
-        :param setPixMap: True/False - set the cell pixmap as required
+        :param setPixMapState: True/False - set the cell pixmap as required
         :return:
         """
         self._button.setVisible(visible)
