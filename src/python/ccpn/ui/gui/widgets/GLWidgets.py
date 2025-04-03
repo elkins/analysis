a51477f088eb7ac3e60da54ad9bb25c22f5fe28f
Ed Brooksbank <ejb66@le.ac.uk>
2025-04-03 10:16:23 +0100

    sometimes reading from the wrong z-plane

--- src/python/ccpn/ui/gui/widgets/GLWidgets.py
+++ src/python/ccpn/ui/gui/widgets/GLWidgets.py
@@ -5,7 +5,7 @@ Module Documentation here
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
-__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2024"
+__copyright__ = "Copyright (C) CCPN project (https://www.ccpn.ac.uk) 2014 - 2025"
 __credits__ = ("Ed Brooksbank, Morgan Hayward, Victoria A Higman, Luca Mureddu, Eliza Płoskoń",
                "Timothy J Ragan, Brian O Smith, Daniel Thompson",
                "Gary S Thompson & Geerten W Vuister")
@@ -17,8 +17,8 @@ __reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, 
 # Last code modification
 #=========================================================================================
 __modifiedBy__ = "$modifiedBy: Ed Brooksbank $"
-__dateModified__ = "$dateModified: 2024-09-09 19:03:26 +0100 (Mon, September 09, 2024) $"
-__version__ = "$Revision: 3.2.6 $"
+__dateModified__ = "$dateModified: 2025-04-03 10:16:24 +0100 (Thu, April 03, 2025) $"
+__version__ = "$Revision: 3.2.12 $"
 #=========================================================================================
 # Created
 #=========================================================================================
@@ -398,9 +398,8 @@ class GuiNdWidget(CcpnGLWidget):
         # generate the new axis labels based on the visible spectrum axisCodes
         self._buildAxisCodesWithWildCards()
 
-        minList = [self._spectrumSettings[sp].ppmPerPoint
-                   for sp in self._ordering if sp in self._spectrumSettings]
-
+        minList = [specView.ppmPerPoints[2:]  # get the z-plane values
+                   for specView in self._ordering if specView in self._spectrumSettings]
         minimumValuePerPoint = None
 
         # check the length of the min values, may have lower dimension spectra overlaid
